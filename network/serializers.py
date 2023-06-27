3ccc93518d75279ccd1f1b9c2a4285ba360b422d
ReshanCSX <reshan.luck@gmail.com>
2023-06-27 13:34:07 +0530

    view posts

--- network/serializers.py
+++ network/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
 from .models import Posts
+from django.contrib.humanize.templatetags.humanize import naturaltime
 
 class PostSerializer(serializers.ModelSerializer):
+
+    timestamp = serializers.SerializerMethodField()
+    author_name = serializers.SerializerMethodField()
     
     class Meta:
         model = Posts
-        fields = ['id', 'content', 'timestamp', 'author']
+        fields = '__all__'
+
+    def get_author_name(self, object):
+        return object.author.username
+    
+    def get_timestamp(self, object):
+        return naturaltime(object.timestamp)
+
+        
